6d3ac2641e073e94e546b8f6ae4d5af8e537fb87
sprinzs123 <sprinzs351@gmail.com>
2020-09-01 18:40:13 -0500

    cloudSQL instance added

--- translate/settings.py
+++ translate/settings.py
@@ -78,12 +78,37 @@ WSGI_APPLICATION = 'translate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': '/cloudsql/about-me-281500:us-east4:serve-me',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'se081q4xJyByln3c',
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'se081q4xJyByln3c',
+        }
+    }
 
 
 # Password validation
